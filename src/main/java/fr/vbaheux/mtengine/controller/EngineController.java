09ef243aa6457cc180a6aff6ab7e31bed847ff02
Vincent Baheux <vince.baheux@gmail.com>
2026-06-22 12:56:01 +0200

    feat: add api to get all modes of a given key

--- src/main/java/fr/vbaheux/mtengine/controller/EngineController.java
+++ src/main/java/fr/vbaheux/mtengine/controller/EngineController.java
@@ -12,9 +12,14 @@ import fr.vbaheux.mtengine.entity.note.Note;
 import fr.vbaheux.mtengine.entity.scale.Scale;
 import fr.vbaheux.mtengine.entity.scale.ScaleQuality;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 public class EngineController {
@@ -30,9 +35,23 @@ public class EngineController {
     return conversionService.convert(Scale.of(key, quality), ScaleDto.class);
   }
 
-  @GetMapping("/mode")
-  public ModeDto mode(@RequestParam(defaultValue = "C") Note key,
-                      @RequestParam(defaultValue = "IONIAN") ModeQuality quality) {
+  @GetMapping("/modes/{key}")
+  @SuppressWarnings("unchecked")
+  public List<ModeDto> modes(@PathVariable Note key) {
+    List<Mode> modes = new LinkedList<>();
+    for (ModeQuality quality : ModeQuality.values()) {
+      modes.add(Mode.of(key, quality));
+    }
+    return (List<ModeDto>) conversionService.convert(
+        modes,
+        TypeDescriptor.collection(List.class, TypeDescriptor.valueOf(Mode.class)),
+        TypeDescriptor.collection(List.class, TypeDescriptor.valueOf(ModeDto.class))
+    );
+  }
+
+  @GetMapping("/modes/{key}/{quality}")
+  public ModeDto mode(@PathVariable Note key,
+                      @PathVariable ModeQuality quality) {
     return conversionService.convert(Mode.of(key, quality), ModeDto.class);
   }
 
